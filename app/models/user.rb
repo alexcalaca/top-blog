69377bd677f45a67d691fa0b635a18b0d6ce5b3a
Rails Udemy Course <alexcalaca@gmail.com>
2018-10-17 21:16:47 +0000

    Add user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,13 @@
 class User < ApplicationRecord
+  validates :username, presence: true,
+                      uniqueness: { case_sensitive: false },
+                      length: { within: 3..30}
+  
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  
+  validates :email, presence: true,
+                    uniqueness: true,
+                    format: { with: VALID_EMAIL_REGEX }
+  
+  
 end
