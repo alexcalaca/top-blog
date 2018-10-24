1e3459a22b820b575c89a24290ea921609077e1b
Rails Udemy Course <alexcalaca@gmail.com>
2018-10-24 13:36:25 +0000

    Add sign in upon sign on

--- README.md
+++ README.md
@@ -5,4 +5,10 @@
 **Pagination**
 will_paginate and bootstrap-will_paginate
 
+Layout style
+Bootstrap 4.1 (gem which bootstrap)
+
+Authentication
+From scratch
+
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,8 +10,9 @@ class UsersController < ApplicationController
     @user = User.new(users_params)
     
     if @user.save
+      session[:user_id] = @user.id
       flash[:success] = "Welcome to the Top Blog #{@user.username}"
-      redirect_to articles_path
+      redirect_to user_path(@user)
     else
       render 'new'
     end
