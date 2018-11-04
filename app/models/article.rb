f9eb2ef13e65723b7d56e7a5843ac41db84951bc
Rails Udemy Course <alexcalaca@gmail.com>
2018-11-04 11:46:30 +0000

    Create association between article and category

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,8 @@
 class Article < ApplicationRecord
   belongs_to :user
+  has_many :article_categories
+  has_many :categories, through: :article_categories
+  
   validates :title, presence: true, length: { within: 3..10}
   validates :description, presence: true, length: { within: 10..300}
   validates :user_id, presence: true
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,6 @@
 class Category < ActiveRecord::Base
+  has_many :article_categories
+  has_many :articles, through: article_categories
+  
   validates :name, presence: true, uniqueness: true, length: { within: 3..25}
 end
