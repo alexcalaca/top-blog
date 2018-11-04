4a729a1cf6fff4633e776aba6bcd63ad9ed735d1
Rails Udemy Course <alexcalaca@gmail.com>
2018-11-04 13:01:40 +0000

    Update test files

--- test/controllers/categories_controller_test.rb
+++ test/controllers/categories_controller_test.rb
@@ -2,12 +2,18 @@ require 'test_helper'
 
 class CategoriesControllerTest < ActionDispatch::IntegrationTest
   
+  def setup
+    @category = Category.create(name: "sports")
+    @user = User.create(username: "john", email: "john@test.com", password: "password", admin: true)
+  end
+  
   test "should get categories index" do 
     get "/categories"
     assert_response :success
   end
   
   test "should get new" do
+    sign_in_as(@user, "password")
     get "/categories/new"
     assert_response :success
   end
@@ -15,5 +21,12 @@ class CategoriesControllerTest < ActionDispatch::IntegrationTest
   test "should get show" do
     get "/categories/id"
     assert_response :success
+  end
+  
+  test "should redirect create when admin not logged in" do
+    assert_no_difference 'Category.count' do
+      post categories_path, params: {category: {name: "sports"}}
+    end
+    assert_redirected_to categories_path
   end
 end
--- test/integration/list_categories_test.rb
+++ test/integration/list_categories_test.rb
@@ -13,20 +13,17 @@ class ListCategoriesTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", category_path(@category), text: @category.name
     assert_select "a[href=?]", category_path(@category2), text: @category2.name
   end
-    
-    
-    
 
   
   test "get new category form and create category" do
     get new_category_path
     assert_template 'categories/new'
     assert_difference 'Category.count', 1 do
-      post categories_path, params: { category: {name: "sports"}}
+      post categories_path, params: { category: {name: "sportss"}}
       follow_redirect!
     end
       assert_template 'categories/index'
-      assert_match "sports", response.body
+      assert_match "sportss", response.body
   end
   
   test "invalid category submission results in failure" do
