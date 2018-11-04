50e3fe87e45dee569a30608bb9c899de48208622
Rails Udemy Course <alexcalaca@gmail.com>
2018-11-04 18:56:28 +0000

    Update show action for articles controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -36,6 +36,8 @@ class ArticlesController < ApplicationController
   end
   
   def show
+    @category = Category.find(params[:id])
+    @category_articles = @category.articles.paginate(page: params[:page], per_page: 5)
   end
   
   def index
